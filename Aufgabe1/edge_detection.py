07fac8ba87148a2690afe33aae72585409b476b9
tamaro <tamaro.walter@uni-muenster.de>
2026-02-14 14:02:40 +0100

    add edge detection with sobel

--- Aufgabe1/edge_detection.py
+++ Aufgabe1/edge_detection.py
@@ -1,15 +1,51 @@
-# File with functions for edge detection.
+"""
+Edge detection module.
+Provides functions for Canny edge detection using Sobel operators.
+
+:author: Tamaro Walter, Simon Schoenhoeffer
+:group: Praktikum Gruppe 14
+"""
 import numpy as np
 
-def sobel(matrix):
-  sobel_y = np.array([
+def sobel(matrix: np.ndarray) -> np.ndarray:
+  # Define sobel operator.
+  sobel_y: np.ndarray = np.array([
     [1, 2, 1,],
     [0, 0, 0,],
     [-1, -2, -1,],
   ])
-
-  sobel_x = np.array([
+  sobel_x: np.ndarray = np.array([
     [-1, 0, 1,],
     [-2, 0, 2,],
     [-1, 0, 1,],
   ])
+
+  rows, cols = matrix.shape
+  edge_strengths: np.ndarray = np.zeros((rows-2, cols-2), dtype=np.uint8)
+
+  for y in range(1, rows-1):
+    for x in range(1, cols -1):
+      # Build a submatrix of the current neighborhood.
+      neighbourhood: np.ndarray = np.array([
+        [matrix[y-1, x-1], matrix[y-1, x], matrix[y-1, x+1] ],
+        [matrix[y, x-1], matrix[y, x], matrix[y, x+1] ],
+        [matrix[y+1, x-1], matrix[y+1, x], matrix[y+1, x+1] ],
+      ])
+      # Compute hx and hy.
+      hy: int = calc_correlation_value(sobel_y, neighbourhood)
+      hx: int = calc_correlation_value(sobel_x, neighbourhood)
+      # Compute the edge strength, rounded to an integer.
+      #edge_strengths[y-1, x-1] = min(int(np.sqrt(hx**2 + hy**2)), 255)     -> genauer Wert
+      edge_strengths[y-1, x-1] = abs(hx) + abs(hy)                        # -> gerundeter Wert wie in der Uebung
+  return edge_strengths
+
+
+
+"""
+Calculates the single correlation value for two 3x3 matrix
+"""
+def calc_correlation_value(m1: np.ndarray, m2:np.ndarray) -> int:
+  row1: int = m1[0,0]*m2[0,0] + m1[0,1]*m2[0,1] + m1[0,2]*m2[0,2]  
+  row2: int = m1[1,0]*m2[1,0] + m1[1,1]*m2[1,1] + m1[1,2]*m2[1,2]
+  row3: int = m1[2,0]*m2[2,0] + m1[2,1]*m2[2,1] + m1[2,2]*m2[2,2]
+  return (row1 + row2 + row3)
--- Aufgabe1/main.py
+++ Aufgabe1/main.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 import glob
-
+import edge_detection as ed
 '''
 Was ist die Aufgabe?
 - Es soll eine Kantenerkennung eines Bildes mit "Canny Edge Detection" implementiert werden.
@@ -16,9 +17,9 @@ Idee der Implementation:
 - Ausgabe: eine Matrix mit den Kanten nach der Hyteresis-Threshold-Operation
 
 Algorithmus:
-1. Schritt: Jpg zu grauwerte-Matrix umwandeln
-2. Schritt: Mit Sobel-Operator Kantenstärke ermitteln
-3. Schritt: Richtung der Kanten bestimmen
+1. Schritt: Jpg zu grauwerte-Matrix umwandeln   -> check
+2. Schritt: Mit Sobel-Operator Kantenstärke ermitteln ->check
+3. Schritt: Richtung der Kanten bestimmen (wird glaub ich nicht in vorlesung behandelt)
 4. Schritt: Non-maximum Suppression anwenden um geeignete Kanten zu finden
 5. Schritt: Hysteresis-Threshold-Operation verwenden um Kanten zu identifizieren
 6. Schritt: Matrix wieder zu einer Jpg umwandeln, Konturen als schwarze Striche
@@ -27,7 +28,22 @@ Wie man dann testen könnte:
 1. Schritt: Vom gleichen bild die groundTruth-Matlab datei einlesen und die Matrix mit der eigens berechneten Matrix vergleichen
 '''
 path = glob.glob("/home/tamaro/Documents/master_informatik/Praktische_Informatik/ComputerVision/Praktikum/BSDS500/BSDS500/data/images/**/2018.*", recursive=True)
-print(path)
+#print(path)
 
-gray_value_matrix = cv2.imread(path[0], cv2.IMREAD_GRAYSCALE)
-print(gray_value_matrix[:5, :5])
+#gray_value_matrix: np.ndarray = cv2.imread(path[0], cv2.IMREAD_GRAYSCALE)
+# Test from exercise 1.5
+gray_value_matrix: np.ndarray = np.array([
+  [3, 1, 5, 7, 2],
+  [2, 1, 6, 5, 6],
+  [1, 0, 7, 6, 7],
+  [0, 1, 2, 6, 5],
+  [2, 1, 0, 1, 6],
+])
+edges = ed.sobel(gray_value_matrix)
+print(edges)
+'''
+expected output:
+[[18 22  8]
+ [24 28  6]
+ [12 34 30]]
+'''
